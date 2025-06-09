393113d690109f29dce31f32f969b93f0c0dae21
Jerry1098 <jeremias1098@mailbox.org>
2025-06-09 12:26:06 +0000

    doc: adds doc for netdata

--- openthread/src/netdata.rs
+++ openthread/src/netdata.rs
@@ -1,4 +1,4 @@
-use core::{fmt, net::Ipv6Addr};
+use core::{fmt::{self, Display}, net::Ipv6Addr};
 
 use openthread_sys::{
     otBorderRouterConfig, otError_OT_ERROR_NONE, otNetDataGetNextOnMeshPrefix,
@@ -8,19 +8,59 @@ use openthread_sys::{
 use crate::{OpenThread, OtError};
 
 
+pub enum OtRoutePreference {
+    /// Low route preference
+    OtRoutePreferenceLow = -1,
+    /// Medium route preference
+    OtRoutePreferenceMed = 0,
+    /// High route preference
+    OtRoutePreferenceHigh = 1,
+    Unkown = 2
+}
+
+impl OtRoutePreference {
+    fn from_ot_int(input: i32) -> Self{
+        match input {
+            -1 => OtRoutePreference::OtRoutePreferenceLow,
+            0 => OtRoutePreference::OtRoutePreferenceMed,
+            1 => OtRoutePreference::OtRoutePreferenceHigh,
+            _ => OtRoutePreference::Unkown
+        }
+    }
+}
+
+impl Display for OtRoutePreference {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "OtRoutePreference: {}", self)
+    }
+}
+
+/// Represents a Border Router configuration
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct OtBorderRouterConfig {
+    /// The IPv6 prefix
     pub prefix: (Ipv6Addr, u8),
-    pub preference: i32,
+    /// A 2-bit signed int preference
+    pub preference: OtRoutePreference,
+    /// Whether prefix is preferred
     pub prefered: bool,
+    /// Whether prefix can be used for address auto-configuration (SLAAC)
     pub slaac: bool,
+    /// Whether border router is DHCPv6 Agent
     pub dhcp: bool,
+    /// Whether DHCPv6 Agent supplying other config data
     pub configure: bool,
+    /// Whether border router is a default router for prefix
     pub default_route: bool,
+    /// Whether this prefix is considered on-mesh
     pub on_mesh: bool,
+    /// Whether this configuration is considered Stable Network Data
     pub stable: bool,
+    /// Whether this border router can supply DNS information via ND
     pub nd_dns: bool,
+    /// Whether prefix is a Thread Domain Prefix (added since Thread 1.2)
     pub domain_prefix: bool,
+    /// The border router's RLOC16 (value ignored on config add)
     pub rloc16: u16,
 }
 
@@ -31,7 +71,7 @@ impl OtBorderRouterConfig {
                 unsafe { config.mPrefix.mPrefix.mFields.m8 }.into(),
                 config.mPrefix.mLength,
             ),
-            preference: config.mPreference(),
+            preference: OtRoutePreference::from_ot_int(config.mPreference()),
             prefered: config.mPreferred(),
             slaac: config.mSlaac(),
             dhcp: config.mDhcp(),
@@ -82,6 +122,12 @@ impl fmt::Display for OtBorderRouterConfig {
 }
 
 impl<'a> OpenThread<'a> {
+    /// Gets the list of all on mesh prefixes
+    /// 
+    /// Arguments:
+    /// - `f`: A closure that will be called for each mesh prefix with the corresponding
+    ///     `OtBorderRouterConfig`. Once called for all prefixes, 
+    ///     the closure will be called with `None`.
     pub fn netdata_get_on_mesh_prefixes<F>(&self, mut f: F) -> Result<(), OtError>
     where
         F: FnMut(Option<OtBorderRouterConfig>) -> Result<(), OtError>,
